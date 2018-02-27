3e3175b2f45e2f2c26753769bc0caeb2a727036f
Kealan McCusker <kealan.mccusker@miracl.com>
2018-02-27 14:48:38 +0000

    updated start script

--- test/request/start.sh
+++ test/request/start.sh
@@ -12,7 +12,7 @@
 echo "make client request"
 sleep 200
 
-/usr/bin/curl -i -H "Content-Type: application/json" -X POST http://localhost:8000/status > response.txt
+/usr/bin/curl -i -H "Content-Type: application/json" -X POST http://localhost:8001/status > response.txt
 grep "HTTP/1.1 200 OK" ./response.txt
 if [ $? = 0 ]; then
     echo "TEST1 PASSED"
@@ -20,7 +20,7 @@ else
     echo "TEST1 FAILED"
 fi
 
-/usr/bin/curl -i -H "Content-Type: application/json" -X POST -d '{"data": "testuser"}' http://localhost:8000/data > response.txt
+/usr/bin/curl -i -H "Content-Type: application/json" -X POST -d '{"data": "testuser"}' http://localhost:8001/data > response.txt
 grep "HTTP/1.1 200 OK" ./response.txt
 if [ $? = 0 ]; then
     echo "TEST2 PASSED"
@@ -28,7 +28,7 @@ else
     echo "TEST2 FAILED"
 fi
 
-/usr/bin/curl -i -H "Content-Type: application/json" -X POST -d '{"data": "testuser"}' http://localhost:8000/baddata  > response.txt
+/usr/bin/curl -i -H "Content-Type: application/json" -X POST -d '{"data": "testuser"}' http://localhost:8001/baddata  > response.txt
 grep "HTTP/1.1 400 Bad Request" ./response.txt
 if [ $? = 0 ]; then
     echo "TEST3 PASSED"
